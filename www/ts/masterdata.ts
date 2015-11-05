e1e94b99bb447f807b6713cd315bbfba154dedb0
yodamad <mvincent.it@gmail.com>
2015-11-05 17:59:37 +0100

    Add additional data to masterdata & json object for server calls

--- www/ts/masterdata.ts
+++ www/ts/masterdata.ts
@@ -1,8 +1,12 @@
 /**
  * Created by mvincent on 02/11/2015.
  */
-
 export class Masterdata {
     public label: string;
     public type: string;
+    public additionalData: string;
+
+    toString():string {
+        return this.type + '_' + this.label;
+    }
 }
