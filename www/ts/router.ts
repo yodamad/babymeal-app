8add132d069ab9de55f1cbc7b495166b18e7b3db
yodamad <mvincent.it@gmail.com>
2015-11-12 18:29:53 +0100

    Add home page

--- www/ts/router.ts
+++ www/ts/router.ts
@@ -7,22 +7,25 @@ import {bootstrap, Component, View, FORM_DIRECTIVES, CORE_DIRECTIVES, provide} f
 import {HTTP_PROVIDERS, Http, Headers} from 'angular2/http';
 import {RouteConfig,  ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppComponent} from './main';
+import {HomeComponent} from './home';
 import {HistoryComponent} from './history';
 import {PoopComponent} from './poop';
 
 @Component({
     selector: 'app',
-    templateUrl: './templates/main.html',
+    templateUrl: './templates/router.html',
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
+    {path: "/Home", component: HomeComponent, as: "Home"},
     {path: "/Bibber", component: AppComponent, as: "Bibber"},
     {path: "/History", component: HistoryComponent, as: "History"},
-    {path: "/Poop", component: PoopComponent, as: "Poop"}
+    {path: "/Poop", component: PoopComponent, as: "Poop"},
 ])
 export class AppRouter {
     constructor() {
         console.log('Init router');
+        location.href = "#/Home";
     }
 }
 bootstrap(AppRouter, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
